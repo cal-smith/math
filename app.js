bfaedc2c2e18e8afd47ad109c1cdcc40dfe72d2d
Callum Smith <callum1234moo@gmail.com>
2014-05-09 18:42:52 -0400

    defaulted back to heroku. going to need to fork
    for openshift

--- app.js
+++ app.js
@@ -70,15 +70,10 @@ io.sockets.on('connection', function(socket) {
 		socket.emit('created', {"id":id});
   });
 });
-
-var port = Number(process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT  || process.env.PORT || 5000);
-if (process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP){
-	var ip = process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1' || 'localhost';
-	server.listen(port, ip, function() {
-  		console.log("Listening on " + port);
-	});
-} else{
-	server.listen(port, function() {
-  		console.log("Listening on " + port);
-	});
-}
+//process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT  ||
+//var ip = process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1' || 'localhost';
+//server.listen(port, ip, function() {
+var port = Number(process.env.PORT || 5000);
+server.listen(port, function() {
+	console.log("Listening on " + port);
+});
